5cdef1d3df014ce37a1649419805d475958c7cb4
unknown <mahammairaj912@gmail.com>
2024-03-23 22:27:37 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1) ;
+
+const answers = await inquirer.prompt([
+  {
+
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please inert a number between 1-10",
+  },
+ 
+]);
+if (answers.userGuessedNumber === randomNumber){
+    console.log("yes you are correct!");
+}
+else if (answers.userGuessedNumber > randomNumber){
+    console.log("opss your guess is too high")
+}
+else if (answers.userGuessedNumber < randomNumber){
+    console.log("your guess is too low!")
+}
+else {
+     console.log("invalid information")
+}
